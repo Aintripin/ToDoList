5d644dab2ac92630e6aa8cf3f0a7ae844ba793a1
Aintripin <yelisey_toyota@mail.ru>
2022-05-02 10:59:21 +0300

    finally fixed some bugs

--- toDoList/src/views/App/index.tsx
+++ toDoList/src/views/App/index.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import styles from "./index.module.scss";
+import { useToDoStore } from "../../data/stores/useToDoStore";
 
 export const App: React.FC = () => {
+    
+    console.log(useToDoStore);
+
+
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask,
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask,
+    ]);
+
+    createTask("sample text");
+
     return (
-        <article>
-            Works!
+        <article className = {styles.article}>
+            <h1 className = {styles.articleTitle}>ToDo App</h1>
+            <section className = {styles.articleSection}>
+
+            </section>
+
+            <section className = {styles.articleSection}>
+
+            </section>
         </article>
     );
 };
