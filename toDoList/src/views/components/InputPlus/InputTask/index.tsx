5f43a3b6c6cc0e65c478c7c59004b64733235460
Aintripin <yelisey_toyota@mail.ru>
2022-05-07 18:40:26 +0300

    added some logic, fixed some bugs

--- toDoList/src/views/components/InputPlus/InputTask/index.tsx
+++ toDoList/src/views/components/InputPlus/InputTask/index.tsx
@@ -17,10 +17,41 @@ export const InputTask: React.FC<InputTaskProps> = ({
     onRemoved,
 }) => { 
 
+    const [checked, setChecked] = useState(false); 
 
     return (
         <div className = {styles.inputTask}>
-            {title}
+            <label>
+                <input 
+                    type = 'checkbox'
+                    checked = {checked}
+                    className = {styles.inputTaskCheckbox}
+                    onChange = {(e) => {
+                        setChecked(e.target.checked);
+
+                        if (e.target.checked) {
+                            onDone(id);
+                        }
+                    }}
+                />
+                <h3 className = {styles.inputTaskTitle}> {title}</h3>
+            </label>
+            <button 
+                aria-label = "Edit"
+                className = {styles.inputTaskEdit}
+                onClick = {() => {
+                
+                }}
+            />
+            <button 
+                aria-label = "Remove"
+                className = {styles.inputTaskRemove}
+                onClick = {() => { 
+                    if (confirm('Iz you suuuure?')) {
+                        onRemoved(id);
+                    }
+                }}
+            />
         </div>
             )
 }
